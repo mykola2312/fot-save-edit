907ea77b4be3d34b8d53affbbc37534a18c34b00
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-08-21 23:15:04 +0300

    push it

--- src/save.rs
+++ src/save.rs
@@ -1,7 +1,8 @@
 use std::fs;
+use std::str;
 use std::path::Path;
-use std::path::PathBuf;
-use thiserror::Error;
+use anyhow::anyhow;
+use anyhow::Result;
 use inflate::inflate_bytes_zlib;
 
 struct World {
@@ -10,40 +11,35 @@ struct World {
     data: Vec<u8>
 }
 
-#[derive(Error, Debug)]
-enum WorldDecodeError {
-    #[error("inflate error")]
-    InflateError(String)
-}
-
 impl World {
-    fn decode(raw: &[u8], offset: usize, size: usize) -> Result<Self, WorldDecodeError> {
-        let data = match inflate_bytes_zlib(&raw[offset..offset+size]) {
-            Ok(data) => data,
-            Err(e) => return Err(WorldDecodeError::InflateError((e)))
-        };
+    fn decode(raw: &[u8], offset: usize, size: usize) -> Result<Self> {
+        let data = inflate_bytes_zlib(&raw[offset..offset+size])
+            .map_err(|e| anyhow!(e))?;
 
         Ok(Self { offset, size, data })
     }
 }
 
 struct Save {
-    raw: Vec<u8>
-}
-
-#[derive(Error, Debug)]
-enum SaveLoadError {
-    #[error("file error")]
-    FileError(std::io::Error)
+    raw: Vec<u8>,
+    worlds: Vec<World>
 }
 
 impl Save {
-    fn load(path: &Path) -> Result<Self, SaveLoadError> {
-        let raw = match fs::read(path) {
-            Ok(raw) => raw,
-            Err(e) => return Err(SaveLoadError::FileError(e))
-        };
+    fn load(path: &Path) -> Result<Self> {
+        let raw = fs::read(path)?;
+        let mut offsets: Vec<usize> = Vec::new();
+        for i in 0..raw.len()-7 {
+            let keyword = match str::from_utf8(&raw[i..i+7]) {
+                Ok(keyword) => keyword,
+                Err(_) => continue
+            };
 
-        Ok(Self { raw })
+            if keyword == "<world>" {
+                offsets.push(i);
+            }
+        }
+
+        Ok(Self { raw, worlds: Vec::new() })
     }
 }
