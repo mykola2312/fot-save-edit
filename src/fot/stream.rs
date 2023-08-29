aa0990e472e927f557026aa68c9611dbe0cbabbe
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-08-29 23:08:53 +0300

    add visibility in impl ReadStream

--- src/fot/stream.rs
+++ src/fot/stream.rs
@@ -10,32 +10,32 @@ pub struct ReadStream<'a> {
 }
 
 impl<'a> ReadStream<'a> {
-    fn new(raw: &Raw, offset: usize) -> ReadStream {
+    pub fn new(raw: &Raw, offset: usize) -> ReadStream {
         ReadStream {
             raw: raw,
             rdr: Cursor::new(&raw.mem[offset..]),
         }
     }
 
-    fn offset(&self) -> usize {
+    pub fn offset(&self) -> usize {
         self.rdr.position() as usize
     }
 
-    fn skip(&mut self, size: usize) {
+    pub fn skip(&mut self, size: usize) {
         self.rdr.set_position(self.rdr.position() + size as u64);
     }
 
-    fn read<T: Decoder>(&mut self, size: usize) -> Result<T> {
+    pub fn read<T: Decoder>(&mut self, size: usize) -> Result<T> {
         let val = T::decode(&self.raw, self.offset(), size)?;
         self.skip(val.get_enc_size());
         Ok(val)
     }
 
-    fn read_u16(&mut self) -> Result<u16> {
+    pub fn read_u16(&mut self) -> Result<u16> {
         Ok(self.rdr.read_u16::<LittleEndian>()?)
     }
 
-    fn read_u32(&mut self) -> Result<u32> {
+    pub fn read_u32(&mut self) -> Result<u32> {
         Ok(self.rdr.read_u32::<LittleEndian>()?)
     }
 }
