7ac969aff40f8ee43efefe3cd573cc10e33728d2
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-08-30 23:57:03 +0300

    implement SSG (though it's unknown)

--- src/fot.rs
+++ src/fot.rs
@@ -3,6 +3,7 @@ mod fstring;
 mod raw;
 pub mod save;
 mod sgd;
+mod ssg;
 mod stream;
 mod tag;
 mod world;
---
+++ src/fot/ssg.rs
@@ -0,0 +1,31 @@
+use super::decoder::Decoder;
+use super::raw::Raw;
+use super::stream::{ReadStream, WriteStream};
+use super::tag::Tag;
+use anyhow::Result;
+
+#[derive(Debug)]
+pub struct SSG {
+    tag: Tag,
+    unk1: Vec<u8>,
+}
+
+impl Decoder for SSG {
+    fn decode(raw: &Raw, offset: usize, _: usize) -> Result<Self> {
+        let mut rd = ReadStream::new(raw, offset);
+        let tag: Tag = rd.read(0)?;
+        let unk1 = rd.read_bytes(0x14)?;
+        Ok(SSG { tag, unk1 })
+    }
+
+    fn encode(&self) -> Result<Raw> {
+        let mut wd = WriteStream::new(self.get_enc_size());
+        wd.write(&self.tag)?;
+        wd.write_bytes(&self.unk1);
+        Ok(wd.into_raw(0, 0x14))
+    }
+
+    fn get_enc_size(&self) -> usize {
+        self.tag.get_enc_size() + 0x14
+    }
+}
