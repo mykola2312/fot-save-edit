1339b4da9ec080d85ff0599f613fe0f02205cf8a
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-08-31 09:43:45 +0300

    implement more ESH types

--- src/fot/esh.rs
+++ src/fot/esh.rs
@@ -7,6 +7,12 @@ use anyhow::Result;
 use indexmap::IndexMap;
 
 #[derive(Debug)]
+pub struct ESHUnknown {
+    pub data_type: u32,
+    pub data: Vec<u8>
+}
+
+#[derive(Debug)]
 pub struct ESHEntityFlags {
     pub entity_id: u16,
     pub flags: u16
@@ -30,6 +36,7 @@ pub struct ESHRect {
 
 #[derive(Debug)]
 pub enum ESHValue {
+    Unknown(ESHUnknown),
     Bool(bool),
     Float(f32),
     Int(i32),
@@ -39,6 +46,18 @@ pub enum ESHValue {
     EntityFlags(ESHEntityFlags),
     Frame(ESHFrame),
     Rect(ESHRect)
+}
+
+impl ESHValue {
+    const TYPE_BOOL: u32 = 1;
+    const TYPE_FLOAT: u32 = 2;
+    const TYPE_INT: u32 = 3;
+    const TYPE_STRING: u32 = 4;
+    const TYPE_SPRITE: u32 = 8;
+    const TYPE_ESBIN: u32 = 11;
+    const TYPE_ENTTITYFLAGS: u32 = 12;
+    const TYPE_FRAME: u32 = 13;
+    const TYPE_RECT: u32 = 14;
 }
 
 #[derive(Debug)]
