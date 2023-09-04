6d7b9f735bed3ebe84bd23d59434c4722e123297
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2023-09-04 03:03:51 +0300

    update on EntityList decoding

--- src/fot/entitylist.rs
+++ src/fot/entitylist.rs
@@ -18,6 +18,7 @@ pub struct EntityList {
     encoding: EntityEncoding,
     entity_file_tag: Option<Tag>,
     entity_tag: Option<Tag>,
+    enc_size: usize,
 
     types: Vec<FString>,
     entities: Vec<Entity>
@@ -46,5 +47,53 @@ impl EntityList {
 
     pub fn get_type_name(&self, type_idx: usize) -> &FString {
         &self.types[type_idx]
+    }
+}
+
+impl DecoderCtx<EntityEncoding> for EntityList {
+    fn decode(raw: &Raw, offset: usize, size: usize, ctx: EntityEncoding) -> Result<Self> {
+        let mut rd = ReadStream::new(raw, offset);
+        let mut ent_list = EntityList {
+            encoding: ctx,
+            entity_file_tag: None,
+            entity_tag: None,
+            enc_size: 0,
+            types: Vec::new(),
+            entities: Vec::new()
+        };
+
+        Ok(match ctx {
+            EntityEncoding::File => {
+                let mut first = true;
+                while rd.offset() < size {
+                    let tag: Tag = rd.read(0)?;
+                    if (first) {
+                        ent_list.entity_tag = Some(tag);
+                        first = false;
+                    }
+
+                    let ent: Entity = rd.read_opt(0, &mut ent_list)?;
+                    ent_list.entities.push(ent);
+                }
+                
+                ent_list.enc_size = rd.offset() - offset;
+                ent_list
+            },
+
+            EntityEncoding::World => {
+
+
+                ent_list.enc_size = rd.offset() - offset;
+                ent_list
+            }
+        })
+    }
+
+    fn encode(&self, ctx: EntityEncoding) -> Result<Raw> {
+        todo!();
+    }
+
+    fn get_enc_size(&self) -> usize {
+        todo!();
     }
 }
